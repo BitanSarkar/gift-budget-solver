d6aa2820d653428ff51da6c41b1612a7e3c5e2d2
bitanSarkar <bitansarkar12345@gmail.com>
2021-11-20 23:27:50 +0530

    feat : constants added

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const text1 = "If you are in a dilemma to figure out the budget for your gift, this is the place for you.";
+export const text2 = "Write the name of the person about whom you care the most in the world right now";
+export const text3 = "Enter";
+export const text4 = "*Note : If you are looking to have the budget of a gift for a group of people using this application. It is advisable to get the budget with respect to each individual and then take the average of all the budgets.";
+export const text5 = "Gift Budget solver";
+export const text6 = "Relationship health between you and the person for whom you want to but the gift";
+export const text7 = "Event for which you want to but the gift";
+export const text8 = "Provide your monthly monetary in flow";
+export const text9 = "Provide your bare minimum estimate of the budget";
+export const text9 = "Your Budget for the gift should be";
+export const GO = ["GO","RETRY"];
+
